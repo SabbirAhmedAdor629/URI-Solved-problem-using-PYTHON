70aba1f610d84c3d8ba51519e57f52f1bf664895
SabbirAhmed <sabbirgitrepository@gmail.com>
2021-02-06 00:13:19 +0600

    Add files via upload

---
+++ URI 2147 (Galopeira).py
@@ -0,0 +1,13 @@
+x = int(input())
+
+for a in range(x):
+    c = input()
+    j = 0
+    for i in (c):
+        j += 0.01
+    print ("{0:.2f}".format(j))
+
+print ("{0:.2f}".format(j))
+print ("{0:.3f}".format(j))
+print("{0:.3f}".format(j))
+print ("{0;")
